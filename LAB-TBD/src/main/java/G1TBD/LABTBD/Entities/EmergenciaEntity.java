f937d52bcd215cbc68dcaccec46556b0c37f51b8
umibana <hans.villarroel@usach.cl>
2024-04-11 23:19:19 -0400

    Agregada Entity emergencia

---
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/Entities/EmergenciaEntity.java
@@ -0,0 +1,46 @@
+package G1TBD.LABTBD.Entities;
+
+public class EmergenciaEntity {
+
+
+    private Integer id;
+    private boolean estadoEmergencia;
+    private String tituloEmergencia;
+    private String descripcionEmergencia;
+
+    public EmergenciaEntity(boolean estadoEmergencia, String tituloEmergencia, String descripcionEmergencia ) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public boolean isEstadoEmergencia() {
+        return estadoEmergencia;
+    }
+
+    public void setEstadoEmergencia(boolean estadoEmergencia) {
+        this.estadoEmergencia = estadoEmergencia;
+    }
+
+    public String getTituloEmergencia() {
+        return tituloEmergencia;
+    }
+
+    public void setTituloEmergencia(String tituloEmergencia) {
+        this.tituloEmergencia = tituloEmergencia;
+    }
+
+    public String getDescripcionEmergencia() {
+        return descripcionEmergencia;
+    }
+
+    public void setDescripcionEmergencia(String descripcionEmergencia) {
+        this.descripcionEmergencia = descripcionEmergencia;
+    }
+}
