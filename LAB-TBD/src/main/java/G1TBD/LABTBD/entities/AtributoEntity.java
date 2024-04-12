2ef645c0dced4dabb4c4459ea01cb007370aa61e
Niko085 <102837121+Niko085@users.noreply.github.com>
2024-04-11 23:46:44 -0400

    Add files via upload

---
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/entities/AtributoEntity.java
@@ -0,0 +1,13 @@
+package G1TBD.LABTBD.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AtributoEntity {
+    private int idAtributo;
+    private String atributo;
+}
--- LAB-TBD/src/main/java/G1TBD/LABTBD/entities/VoluntarioEntity.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/entities/VoluntarioEntity.java
@@ -1,6 +1,12 @@
 package G1TBD.LABTBD.entities;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class VoluntarioEntity {
     private int idVoluntario;
     private String rutVoluntario;
@@ -11,82 +17,5 @@ public class VoluntarioEntity {
     private String contrasena;
     private boolean disponibilidad;
 
-    public VoluntarioEntity() {
-    }
 
-    public VoluntarioEntity(int idVoluntario, String rutVoluntario, String nombreVoluntario, String apellidoVoluntario, int edadVoluntario, boolean sexoVoluntario, String contrasena, boolean disponibilidad) {
-        this.idVoluntario = idVoluntario;
-        this.rutVoluntario = rutVoluntario;
-        this.nombreVoluntario = nombreVoluntario;
-        this.apellidoVoluntario = apellidoVoluntario;
-        this.edadVoluntario = edadVoluntario;
-        this.sexoVoluntario = sexoVoluntario;
-        this.contrasena = contrasena;
-        this.disponibilidad = disponibilidad;
-    }
-
-    public int getIdVoluntario() {
-        return idVoluntario;
-    }
-
-    public String getRutVoluntario() {
-        return rutVoluntario;
-    }
-
-    public String getNombreVoluntario() {
-        return nombreVoluntario;
-    }
-
-    public String getApellidoVoluntario() {
-        return apellidoVoluntario;
-    }
-
-    public int getEdadVoluntario() {
-        return edadVoluntario;
-    }
-
-    public boolean isSexoVoluntario() {
-        return sexoVoluntario;
-    }
-
-    public String getContrasena() {
-        return contrasena;
-    }
-
-    public boolean isDisponibilidad() {
-        return disponibilidad;
-    }
-
-
-    public void setIdVoluntario(int idVoluntario) {
-        this.idVoluntario = idVoluntario;
-    }
-
-    public void setRutVoluntario(String rutVoluntario) {
-        this.rutVoluntario = rutVoluntario;
-    }
-
-    public void setNombreVoluntario(String nombreVoluntario) {
-        this.nombreVoluntario = nombreVoluntario;
-    }
-
-    public void setApellidoVoluntario(String apellidoVoluntario) {
-        this.apellidoVoluntario = apellidoVoluntario;
-    }
-
-    public void setEdadVoluntario(int edadVoluntario) {
-        this.edadVoluntario = edadVoluntario;
-    }
-
-    public void setSexoVoluntario(boolean sexoVoluntario) {
-        this.sexoVoluntario = sexoVoluntario;
-    }
-
-    public void setContrasena(String contrasena) {
-        this.contrasena = contrasena;
-    }
-
-    public void setDisponibilidad(boolean disponibilidad) {
-        this.disponibilidad = disponibilidad;
-    }
 }
