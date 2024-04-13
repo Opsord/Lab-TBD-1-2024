27dd85fe8e0511e49aab2ea9cbea224973520036
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-12 22:42:29 -0400

    [Esteban Arenas] Config database

--- LAB-TBD/src/main/java/G1TBD/LABTBD/config/DatabaseConfig.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/config/DatabaseConfig.java
@@ -1,14 +1,23 @@
 package G1TBD.LABTBD.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.sql2o.Sql2o;
 
 @Configuration
 public class DatabaseConfig {
+    @Value("jdbc:postgresql://localhost:5432/LabTBD-1-2024")
+    private String dbUrl;
 
-    private static Sql2o sql2o;
+    @Value("postgres")
+    private String dbUser;
 
-    static{
-        sql2o = new Sql2o("jdbc:mysql://localhost:3306/LabTBD-1-2024", "postgres", "1502");
+    @Value("12345678")
+    private String dbPass;
+
+    @Bean
+    public Sql2o sql2o() {
+        return new Sql2o(dbUrl, dbUser, dbPass);
     }
 }
