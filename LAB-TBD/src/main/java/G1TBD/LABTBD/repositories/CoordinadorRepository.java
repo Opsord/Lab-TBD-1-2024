375f457ff8cad2f38808c34b9a273d1dadb5ee43
umibana <hans.villarroel@usach.cl>
2024-04-12 22:31:31 -0400

    Agregado crud coordinador

--- LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/CoordinadorRepository.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/CoordinadorRepository.java
@@ -1,71 +1,17 @@
 package G1TBD.LABTBD.repositories;
 
 import G1TBD.LABTBD.entities.CoordinadorEntity;
-import org.sql2o.Connection;
-import org.sql2o.Sql2o;
 
-public class CoordinadorRepository {
-    private Sql2o sql2o;
+public interface CoordinadorRepository {
+    // Crear un Coordinador
+    void create(CoordinadorEntity coordinador);
 
-    public CoordinadorRepository(Sql2o sql2o) {
-        this.sql2o = sql2o;
-    }
+    // Conseguir coordinador por su id
+    CoordinadorEntity conseguirPorId(Integer id);
 
-    // Crear un nuevo Coordinador
-    public void add(CoordinadorEntity coordinador) {
-        String sql =
-                "INSERT INTO Coordinador (rutCoordinador, nombreCoordinador, apellidoCoordinador, contrasena, idInstitucion) " +
-                        "VALUES (:rutCoordinador, :nombreCoordinador, :apellidoCoordinador, :contrasena, :idInstitucion)";
-
-        try (Connection con = sql2o.open()) {
-            con.createQuery(sql)
-                    .addParameter("rutcoordinador", coordinador.getRutCoordinador())
-                    .addParameter("nombrecoordinador", coordinador.getNombreCoordinador())
-                    .addParameter("apellidocoordinador", coordinador.getApellidoCoordinador())
-                    .addParameter("contrasena", coordinador.getContrasena())
-                    .addParameter("idinstitucion", coordinador.getIdInstitucion())
-                    .executeUpdate();
-        }
-    }
-
-    // Leer (obtener) un Coordinador por su ID
-    public CoordinadorEntity getById(Integer id) {
-        String sql = "SELECT * FROM Coordinador WHERE idCoordinador = :idCoordinador";
-
-        try (Connection con = sql2o.open()) {
-            return con.createQuery(sql)
-                    .addParameter("idcoordinador", id)
-                    .executeAndFetchFirst(CoordinadorEntity.class);
-        }
-    }
-
-    // Actualizar un Coordinador
-    public void update(CoordinadorEntity coordinador) {
-        String sql =
-                "UPDATE Coordinador SET rutCoordinador = :rutCoordinador, nombreCoordinador = :nombreCoordinador, " +
-                        "apellidoCoordinador = :apellidoCoordinador, contrasena = :contrasena, idInstitucion = :idInstitucion " +
-                        "WHERE idCoordinador = :idCoordinador";
-
-        try (Connection con = sql2o.open()) {
-            con.createQuery(sql)
-                    .addParameter("idcoordinador", coordinador.getIdCoordinador())
-                    .addParameter("rutcoordinador", coordinador.getRutCoordinador())
-                    .addParameter("nombrecoordinador", coordinador.getNombreCoordinador())
-                    .addParameter("apellidocoordinador", coordinador.getApellidoCoordinador())
-                    .addParameter("contrasena", coordinador.getContrasena())
-                    .addParameter("idinstitucion", coordinador.getIdInstitucion())
-                    .executeUpdate();
-        }
-    }
+    // Actualizar datos de un coordinador
+    void Update(CoordinadorEntity coordinador);
 
     // Eliminar un Coordinador
-    public void delete(Integer id) {
-        String sql = "DELETE FROM Coordinador WHERE idCoordinador = :idCoordinador";
-
-        try (Connection con = sql2o.open()) {
-            con.createQuery(sql)
-                    .addParameter("idcoordinador", id)
-                    .executeUpdate();
-        }
-    }
+    void Delete(Integer id);
 }
