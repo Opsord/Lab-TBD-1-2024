43b024e2f77cf355297c61a55fac9c34c2ec42b2
Niko085 <102837121+Niko085@users.noreply.github.com>
2024-04-12 22:40:58 -0400

    [Nicolas Gajardo] crud voluntario

--- LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/VoluntarioRepository.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/VoluntarioRepository.java
@@ -1,4 +1,17 @@
 package G1TBD.LABTBD.repositories;
+import G1TBD.LABTBD.entities.VoluntarioEntity;
 
 public interface VoluntarioRepository {
+    // Crear un voluntario
+    void create(VoluntarioEntity voluntario);
+
+    // Conseguir voluntario por su id
+    VoluntarioEntity conseguirPorId(Integer id);
+
+    // Actualizar datos de un voluntario
+    void Update(VoluntarioEntity voluntario);
+
+    // Eliminar un Coordinador
+    void Delete(Integer id);
+
 }
