7b4468f35385795feee7f00bd2b435cc12fbda7c
Yuyunisse <yuyunisse.carreno@usach.cl>
2024-04-12 23:26:16 -0400

    Institucion
    repository e imp de Institucion

--- LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/InstitucionRepository.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/InstitucionRepository.java
@@ -1,4 +1,23 @@
 package G1TBD.LABTBD.repositories;
 
+import G1TBD.LABTBD.entities.InstitucionEntity;
+
+import java.util.List;
+
 public interface InstitucionRepository {
+
+
+
+    //Crear intitucion
+    void create(InstitucionEntity institucion);
+
+    //Leer instituciones
+    public List<InstitucionEntity> conseguirTodo(InstitucionEntity institucion);
+
+
+    //Actualizar institucion
+    void update(InstitucionEntity institucion);
+
+    //Eliminar intitucion
+    void delete(Integer id);
 }
