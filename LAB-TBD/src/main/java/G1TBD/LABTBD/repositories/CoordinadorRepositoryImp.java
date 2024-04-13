be30668b211214e85153e3b9802e1c7f89f49930
umibana <hans.villarroel@usach.cl>
2024-04-12 22:48:22 -0400

    Database Context como bean y @repository

--- LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/CoordinadorRepositoryImp.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/CoordinadorRepositoryImp.java
@@ -1,15 +1,15 @@
 package G1TBD.LABTBD.repositories;
 
 import G1TBD.LABTBD.entities.CoordinadorEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+@Repository
 public class CoordinadorRepositoryImp implements CoordinadorRepository {
+    @Autowired
     private Sql2o sql2o;
-
-    public CoordinadorRepositoryImp(Sql2o sql2o) {
-        this.sql2o = sql2o;
-    }
 
     // Crear un Coordinador
     @Override
