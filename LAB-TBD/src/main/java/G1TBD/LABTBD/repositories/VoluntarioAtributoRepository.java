2b75ab8a35188828d7b40ddcd0a272c8ca747732
Niko085 <102837121+Niko085@users.noreply.github.com>
2024-04-12 22:44:46 -0400

    [Nicolas Gajardo] crud VoluntarioAtributoRepository

--- LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/VoluntarioAtributoRepository.java
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/repositories/VoluntarioAtributoRepository.java
@@ -1,4 +1,17 @@
 package G1TBD.LABTBD.repositories;
 
+import G1TBD.LABTBD.entities.VoluntarioAtributoEntity;
+
 public interface VoluntarioAtributoRepository {
+    // Crear un voluntarioAtributo
+    void create(VoluntarioAtributoEntity voluntarioAtributo);
+
+    // Conseguir voluntarioAtributo por su id
+    VoluntarioAtributoEntity conseguirPorId(Integer id);
+
+    // Actualizar datos de un voluntarioAtributo
+    void Update(VoluntarioAtributoEntity voluntarioAtributo);
+
+    // Eliminar un VoluntarioAtributo
+    void Delete(Integer id);
 }
