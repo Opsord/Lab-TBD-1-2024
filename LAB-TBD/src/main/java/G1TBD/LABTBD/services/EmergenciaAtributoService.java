b26bfb93bac1add8d3addbd57cd1b3bd68398e88
Niko085 <nicolas.gajardo.p@usach.cl>
2024-04-13 23:03:03 -0400

    [Nicolas Gajardo] actualizacion service

---
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/services/EmergenciaAtributoService.java
@@ -0,0 +1,34 @@
+package G1TBD.LABTBD.services;
+
+import G1TBD.LABTBD.entities.EmergenciaAtributoEntity;
+import G1TBD.LABTBD.repositories.EmergenciaAtributoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmergenciaAtributoService {
+    @Autowired
+    private EmergenciaAtributoRepository emergenciaAtributoRepository;
+
+    public EmergenciaAtributoEntity crearEmergenciaAtributo(EmergenciaAtributoEntity emergenciaAtributo) {
+        return emergenciaAtributoRepository.crearEmergenciaAtributo(emergenciaAtributo);
+    }
+
+    public List<EmergenciaAtributoEntity> obtenerTodasLasEmergenciaAtributo() {
+        return emergenciaAtributoRepository.obtenerTodosLosEmergenciaAtributo();
+    }
+
+    public EmergenciaAtributoEntity obtenerEmergenciaAtributoPorId(long id) {
+        return emergenciaAtributoRepository.obtenerEmergenciaAtributoPorId(id);
+    }
+
+    public boolean actualizarEmergenciaAtributo(EmergenciaAtributoEntity voluntarioAtributo) {
+        return emergenciaAtributoRepository.actualizarEmergenciaAtributo(voluntarioAtributo);
+    }
+
+    public boolean eliminarEmargenciaAtributo(long id) {
+        return emergenciaAtributoRepository.eliminarEmergenciaAtributo(id);
+    }
+}
