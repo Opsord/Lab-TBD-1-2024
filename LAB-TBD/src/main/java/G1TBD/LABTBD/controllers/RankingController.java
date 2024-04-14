aecd4f60d033d846760540e560763b17afd416bb
umibana <hans.villarroel@usach.cl>
2024-04-13 23:34:30 -0400

    Ranking Controller implementado

---
+++ LAB-TBD/src/main/java/G1TBD/LABTBD/controllers/RankingController.java
@@ -0,0 +1,44 @@
+package G1TBD.LABTBD.controllers;
+
+import G1TBD.LABTBD.entities.RankingEntity;
+import G1TBD.LABTBD.repositories.RankingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:8080/ranking")
+public class RankingController {
+    @Autowired
+    private RankingRepository rankingRepository;
+
+    @PostMapping("/crearRanking")
+    public String crearRanking(RankingEntity ranking) {
+        rankingRepository.crearRanking(ranking);
+        return "redirect:/ranking"; // Ajusta según la ruta deseada
+    }
+
+    @GetMapping("/obtenerTodosLosRanking")
+    public List<RankingEntity> obtenerTodosLosRanking() {
+        return rankingRepository.obtenerTodosLosRanking();
+    }
+
+    @GetMapping("/obtenerRankingPorId")
+    public RankingEntity obtenerRankingPorId(long id) {
+        return rankingRepository.obtenerRankingPorId(id);
+    }
+
+    @PostMapping("/actualizarRanking")
+    public boolean actualizarRanking(RankingEntity ranking) {
+        return rankingRepository.actualizarRanking(ranking);
+    }
+
+    @PostMapping("/eliminarRanking")
+    public boolean eliminarRanking(long id) {
+        return rankingRepository.eliminarRanking(id);
+    }
+}
