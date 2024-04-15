8067307be93d92ac81f82c8b84c512b08d0d9ec1
Niko085 <nicolas.gajardo.p@usach.cl>
2024-04-14 23:50:24 -0400

    [Nicolas Gajardo] Poblacion terminada

--- poblacion.sql
+++ poblacion.sql
@@ -70,6 +70,45 @@ VALUES
     (4, true, 'Accidente de tráfico múltiple', 'Se ha producido un accidente de tráfico en la autopista principal. Varios vehículos están involucrados.', 1),
     (5, false, 'Deslizamiento de tierra en carretera', 'Un deslizamiento de tierra ha bloqueado una carretera importante. Se necesita ayuda para despejar la vía.', 3);
 
+--Poblacion de tabla
+-- Población de la tabla Emergencia_Atributo
+INSERT INTO Emergencia_Atributo (idEmergenciaAtributo, idEmergencia, idAtributo, compatibilidad)
+VALUES 
+    (1, 1, 1, true),   -- La emergencia 1 tiene compatibilidad con el atributo 1 (Fuerza física)
+    (2, 1, 2, true),   -- La emergencia 1 tiene compatibilidad con el atributo 2 (Rescate en Terreno)
+    (3, 1, 3, false),  -- La emergencia 1 no tiene compatibilidad con el atributo 3 (Apoyo psicológico)
+    (4, 2, 4, false),  -- La emergencia 2 no tiene compatibilidad con el atributo 4 (Manejo de herramientas de rescate)
+    (5, 2, 5, true),   -- La emergencia 2 tiene compatibilidad con el atributo 5 (Conocimientos en primeros auxilios)
+    (6, 3, 6, true),   -- La emergencia 3 tiene compatibilidad con el atributo 6 (Manejo de equipos de comunicación)
+    (7, 3, 7, true),   -- La emergencia 3 tiene compatibilidad con el atributo 7 (Capacidad para la búsqueda y localización de personas perdidas)
+    (8, 4, 1, true),   -- La emergencia 4 tiene compatibilidad con el atributo 1 (Fuerza física)
+    (9, 4, 2, false),  -- La emergencia 4 no tiene compatibilidad con el atributo 2 (Rescate en Terreno)
+    (10, 5, 6, false), -- La emergencia 5 no tiene compatibilidad con el atributo 6 (Manejo de equipos de comunicación)
+    (11, 5, 7, true);  -- La emergencia 5 tiene compatibilidad con el atributo 7 (Capacidad para la búsqueda y localización de personas perdidas)
+
+-- Población de la tabla Tarea
+INSERT INTO Tarea (idTarea, idEmergencia, tipoTarea, descripcion, estado)
+VALUES 
+    (1, 1, 'Evacuación de residentes', 'Coordinar y ejecutar la evacuación de los residentes afectados por el incendio forestal.', true),
+    (2, 2, 'Control de fuga de gas', 'Gestionar la contención y control de la fuga de gas en el edificio residencial.', false),
+    (3, 3, 'Rescate de personas atrapadas', 'Realizar operaciones de rescate para ayudar a las personas atrapadas por la inundación.', true),
+    (4, 4, 'Atención médica a heridos', 'Brindar atención médica a los heridos en el accidente de tráfico múltiple.', true),
+    (5, 5, 'Despeje de la carretera', 'Organizar y llevar a cabo el despeje de la carretera bloqueada por el deslizamiento de tierra.', false);
+
+-- Población de la tabla Ranking
+INSERT INTO Ranking (idRanking, idVoluntario, idTarea, valorRanking)
+VALUES 
+    (1, 1, 1, 4),   -- Juan participó en la evacuación de residentes del incendio forestal y recibió un valor de ranking de 4.
+    (2, 2, 2, 3),   -- María estuvo a cargo del control de la fuga de gas y recibió un valor de ranking de 3.
+    (3, 3, 3, 5),   -- Carlos participó en el rescate de personas atrapadas por la inundación y recibió un valor de ranking de 5.
+    (4, 4, 4, 4),   -- Ana brindó atención médica a los heridos en el accidente de tráfico múltiple y recibió un valor de ranking de 4.
+    (5, 5, 5, 2),   -- Pedro ayudó en el despeje de la carretera bloqueada por el deslizamiento de tierra y recibió un valor de ranking de 2.
+    (6, 6, 1, 3),   -- Laura participó en la evacuación de residentes del incendio forestal y recibió un valor de ranking de 3.
+    (7, 7, 2, 4),   -- Diego estuvo a cargo del control de la fuga de gas y recibió un valor de ranking de 4.
+    (8, 8, 3, 5),   -- Sofía participó en el rescate de personas atrapadas por la inundación y recibió un valor de ranking de 5.
+    (9, 9, 4, 4),   -- Miguel brindó atención médica a los heridos en el accidente de tráfico múltiple y recibió un valor de ranking de 4.
+    (10, 10, 5, 3);-- Paula ayudó en el despeje de la carretera bloqueada por el deslizamiento de tierra y recibió un valor de ranking de 3.
+
 
 
 
