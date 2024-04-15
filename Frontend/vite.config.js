430e04eaf3c5a7d0cb64961832379a52e68bb23d
umibana <hans.villarroel@usach.cl>
2024-04-14 22:55:55 -0400

    Agregado shadcn a frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,6 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     vue(),
   ],
