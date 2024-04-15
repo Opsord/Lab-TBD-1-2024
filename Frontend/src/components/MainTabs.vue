b43fabd6c77fc6b8d71d45d69ff37d46e2e1fec9
umibana <hans.villarroel@usach.cl>
2024-04-14 23:34:56 -0400

    Mockup frontend

---
+++ Frontend/src/components/MainTabs.vue
@@ -0,0 +1,23 @@
+<script setup>
+import '../assets/base.css'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+</script>
+
+<template>
+    <Tabs default-value="account" class="w-[400px]">
+        <TabsList>
+            <TabsTrigger value="inicio">
+                Inicio
+            </TabsTrigger>
+            <TabsTrigger value="ingreso">
+                Ingreso de Emergencia
+            </TabsTrigger>
+            <TabsTrigger value="emergencias">
+                Emergencias Activas
+            </TabsTrigger>
+            <TabsTrigger value="finalizadas">
+                Emergencias Finalizadas
+            </TabsTrigger>
+        </TabsList>
+    </Tabs>
+</template>
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import path from "path"
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+
+import tailwind from "tailwindcss"
+import autoprefixer from "autoprefixer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +18,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      // '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": path.resolve(__dirname, "./src"),
     }
   }
 })
