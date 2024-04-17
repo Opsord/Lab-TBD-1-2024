a9457d7e61f038e842b39b9987945d1d3bf99b9b
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-17 10:57:43 -0400

    [Esteban Arenas] Diseño nuevo navbar

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -3,13 +3,14 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import './assets/base.css'
 import MainTabs from "./components/MainTabs.vue"
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
 
   <main>
 
-    <div class="h-screen w-screen  bg-zinc-900 text-zinc-200 p-4">
+<!--     <div class="h-screen w-screen  bg-zinc-900 text-zinc-200 p-4">
       <div class="flex flex-row  justify-center md:mr-56 ">
         <MainTabs />
       </div>
@@ -17,7 +18,9 @@ import MainTabs from "./components/MainTabs.vue"
         <h1 class=" text-4xl"> Sistema de Emergencia Metropolitana</h1>
 
       </div>
-    </div>
+    </div> -->
+
+    <NavBar />
   </main>
   <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
     <div class="wrapper">
---
+++ Frontend/src/components/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div className="container-nav">
+        <div className="container-content">
+            <img src="../assets/logo-sistema.png" alt="">
+            <span>Sistema de Emergencias<span>Región Metropolitana</span></span>
+        </div>
+        <div className="container-content">
+            <img src="../assets/logo-usach.png" alt="">
+        </div>
+    </div>
+</template>
+
+<style>
+
+.container-nav {
+    display: flex;
+    justify-content: space-around;
+    padding: 5px 0;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.container-content {
+    display: flex;
+    align-items: center;
+    gap: 30px;
+}
+
+img {
+    height: 80px;
+}
+
+span {
+    display: flex;
+    flex-direction: column;
+    text-transform: uppercase;
+    font-size: 1.5rem;
+    font-weight: bold;
+}
+
+span:first-child {
+    font-size: 1.1rem;
+    letter-spacing: 5px; 
+}
+
+</style>
