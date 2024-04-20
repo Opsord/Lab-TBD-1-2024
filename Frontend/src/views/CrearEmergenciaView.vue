8968eabdcb5710349e14b3689f12261021bd6ccd
umibana <hans.villarroel@usach.cl>
2024-04-20 00:31:52 -0400

    Removidos imports innecesariso

--- Frontend/src/views/CrearEmergenciaView.vue
+++ Frontend/src/views/CrearEmergenciaView.vue
@@ -12,7 +12,6 @@ import { Checkbox } from '@/components/ui/checkbox';
 import { Button } from '@/components/ui/button';
 
 import { ref } from 'vue';
-import { Textarea } from "@/components/ui/textarea"
 import ScrollArea from '@/components/ui/scroll-area/ScrollArea.vue';
 
 // Separar esto en reqeust para emregencias y otro para los atributos
@@ -31,7 +30,6 @@ const habilidadModel = ref(
 
 import axios from 'axios'
 import { onMounted } from 'vue'
-import { Compass } from 'lucide-vue-next';
 
 const atributos = ref(null)
 
@@ -46,10 +44,6 @@ async function fetchAtributos() {
 
 // If you want to fetch data when the component mounts
 onMounted(fetchAtributos);
-// Tengo como idea para marcar los que no necesitan
-// Solo agregar otro textbox donde se indica que NO se necesita
-// El input de atributo será separado por commas
-// Para mandar a backend, tomo estos dos datos del json, si viene de SI, marcar con 1 sino 0
 
 function onSubmit() {
     console.log('Form submitted!', JSON.stringify(formModel.value, null, 2))
