f21628f1dbeb52a7c7da24e42e569d3dd7a94d6d
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-20 16:23:55 -0400

    [Esteban Arenas] Correción espaciado entre componentes LoginView

--- Frontend/src/views/LoginView.vue
+++ Frontend/src/views/LoginView.vue
@@ -6,16 +6,16 @@
         </div>
         <div class="w-full p-8 bg-gray-50 border rounded-xl shadow-md">
             <h2 class="mb-8 text-center">Por favor, ingresa tus datos</h2>
-            <form>
-                <div class="flex flex-col gap-2 mb-2">
+            <form class="space-y-5">
+                <div class="flex flex-col gap-2">
                     <label for="correo" class="text-sm">Correo electrónico</label>
                     <input v-model="correo" type="email" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="usuario@email.com"/>
                 </div>
-                <div class="flex flex-col gap-2 mb-2">
+                <div class="flex flex-col gap-2">
                     <label for="contrasena" class="text-sm">Contraseña</label>
                     <input v-model="contrasena" type="password" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="********">
                 </div>
-                <button class="w-full px-3 py-2 my-5 bg-amber-500 text-white font-medium uppercase tracking-widest rounded-full hover:bg-amber-600">
+                <button class="w-full px-3 py-2 bg-amber-500 text-white font-medium uppercase tracking-widest rounded-full hover:bg-amber-600">
                     Ingresar
                 </button>
                 <div class="flex justify-center gap-2 text-sm">
