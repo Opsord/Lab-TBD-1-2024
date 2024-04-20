4c62b1e5f7444313382f502fef81f429265a7a89
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-20 16:17:45 -0400

    [Esteban Arenas] Vista login terminada, sin logica ni conexion con backend

--- Frontend/src/views/LoginView.vue
+++ Frontend/src/views/LoginView.vue
@@ -1,26 +1,26 @@
 <template>
-    <div class="flex flex-col justify-center items-center gap-8 w-2/4 m-auto">
+    <div class="flex flex-col justify-center items-center gap-8 m-auto w-1/5">
         <div class="flex flex-col justify-center items-center gap-6">
             <img class="w-24 h-24" src="../assets/logo-sistema.png" alt="">
             <h2 class=" text-lg font-bold text-teal-600">¡Es un buen día para ayudar!</h2>
         </div>
-        <div class="p-8 bg-gray-50 border rounded-xl shadow-md">
+        <div class="w-full p-8 bg-gray-50 border rounded-xl shadow-md">
             <h2 class="mb-8 text-center">Por favor, ingresa tus datos</h2>
             <form>
-                <div class="">
-                    <label for="correo">Correo electrónico</label>
-                    <input v-model="correo" type="email"/>
+                <div class="flex flex-col gap-2 mb-2">
+                    <label for="correo" class="text-sm">Correo electrónico</label>
+                    <input v-model="correo" type="email" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="usuario@email.com"/>
                 </div>
-                <div class="form-group">
-                    <label for="contrasena">Contraseña</label>
-                    <input v-model="contrasena" type="password" />
+                <div class="flex flex-col gap-2 mb-2">
+                    <label for="contrasena" class="text-sm">Contraseña</label>
+                    <input v-model="contrasena" type="password" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="********">
                 </div>
-                <button class="button">
+                <button class="w-full px-3 py-2 my-5 bg-amber-500 text-white font-medium uppercase tracking-widest rounded-full hover:bg-amber-600">
                     Ingresar
                 </button>
-                <div class="">
-                    <span>Aún no tienes cuenta? </span>
-                    <a href="">Registrate aquí</a>
+                <div class="flex justify-center gap-2 text-sm">
+                    <span>¿Aún no tienes cuenta?</span>
+                    <a href="" class="font-bold text-teal-600 hover:underline">Registrate</a>
                 </div>
             </form>
         </div>
