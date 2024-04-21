91396e296e3a770ee7ba63b39c414c43636d0776
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-21 14:13:15 -0400

    [Esteban Arenas] Logica principal register

--- Frontend/src/views/RegisterView.vue
+++ Frontend/src/views/RegisterView.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex flex-col gap-8 mx-auto my-20 w-4/12">
+    <div class="flex flex-col gap-8 mx-auto my-16 w-4/12">
         <div class="flex justify-center">
             <h2 class=" text-lg font-bold text-teal-600">¿Quieres formar parte de nosotros?</h2>
         </div>
@@ -8,14 +8,20 @@
             <form>
                 <div class="grid gap-5 grid-cols-2">
                     <div class="flex flex-col gap-2">
-                        <label for="sexo" class="text-sm">Rol</label>
-                        <select class="px-3 py-2 border border-gray-400 rounded text-sm">
+                        <label for="rol" class="text-sm">Rol</label>
+                        <select  v-model="rol" class="px-3 py-2 border border-gray-400 rounded text-sm" required>
                             <option value="">Rol</option>
-                            <option value="">Coordinador</option>
-                            <option value="">Voluntario</option>    
+                            <option value="Coordinador">Coordinador</option>
+                            <option value="Voluntario">Voluntario</option>    
                         </select>
                     </div>
-                    <div></div>
+                    <div class="flex flex-col gap-2">
+                        <label for="edad" class="text-sm">Institucion<span class="text-blue-700 ml-2">* Coordinador</span></label>
+                        <select v-model="institucion" class="px-3 py-2 border border-gray-400 rounded text-sm" required>
+                            <option value="">Institucion</option>s
+                            <option v-for="institucion in instituciones" :key="institucion.idInstitucion" :value="institucion.idInstitucion">{{ institucion.nombreInstitucion }}</option>
+                        </select>
+                    </div>
                     <div class="flex flex-col gap-2">
                         <label for="nombre" class="text-sm">Nombre</label>
                         <Input v-model="nombre" type="text" placeholder="Nombre usuario"/>
@@ -25,7 +31,7 @@
                         <Input v-model="apellido" type="text" placeholder="Apellido usuario"/>
                     </div>
                     <div class="flex flex-col gap-2">
-                        <label for="correo" class="text-sm">Correo electrónico</label>
+                        <label for="email" class="text-sm">Correo electrónico</label>
                         <Input v-model="email" type="email" placeholder="usuario@email.com"/>
                     </div>
                     <div class="flex flex-col gap-2">
@@ -39,33 +45,33 @@
                     <div class="flex justify-between">
                         <div class="flex flex-col gap-2">
                             <label for="edad" class="text-sm">Edad</label>
-                            <select class="w-32 px-3 py-2 border border-gray-400 rounded text-sm">
+                            <select v-model="edad" class="w-32 px-3 py-2 border border-gray-400 rounded text-sm" required>
                                 <option value="">Edad</option>
                                 <option v-for="edad in edades" :key="edad" :value="edad">{{ edad }}</option>
                             </select>
                         </div>
                         <div class="flex flex-col gap-2">
                             <label for="sexo" class="text-sm">Sexo</label>
-                            <select class="w-32 px-3 py-2 border border-gray-400 rounded text-sm">
+                            <select v-model="sexo" class="w-32 px-3 py-2 border border-gray-400 rounded text-sm" required>
                                 <option value="">Sexo</option>
-                                <option value="">Masculino</option>
-                                <option value="">Femenino</option>
-                                <option value="">No binario</option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                                <option value="No binario">No binario</option>
                             </select>
                         </div>
                     </div>
-                    <div class="flex items-end gap-6 my-1">
-                        <label for="disponibilidad" class="text-sm">¿Estás disponible?</label>
+                    <div class="flex items-end gap-4 my-1 flex-wrap">
+                        <label for="disponibilidad" class="w-full text-sm">¿Estás disponible?<span class="text-blue-700"> * Voluntario</span></label>
                         <div class="flex items-center space-x-2">
-                            <input type="radio" name="disponibilidad">
-                            <label for="disponibilidad-si" class="text-sm">Sí</label>
+                            <input v-model="disponibilidad" type="radio" name="disponibilidad" required>
+                            <label for="disponibilidadsi" class="text-sm">Sí</label>
                         </div>
                         <div class="flex items-center space-x-2">   
-                            <input type="radio" name="disponibilidad">
-                            <label for="disponibilidad-no" class="text-sm">No</label>
+                            <input v-model="disponibilidad" type="radio" name="disponibilidad" required>
+                            <label for="disponibilidadno" class="text-sm">No</label>
                         </div>
                     </div>
-                    <ButtonPrimary @click="redirectToLogin" buttonText="Registrarse"/>
+                    <ButtonPrimary @click="registerUser" buttonText="Registrarse"/>
                 </div>
             </form>
         </div>
@@ -81,27 +87,6 @@ import Input from "../components/Input.vue";
 
 const router = useRouter();
 
-const edades = ref([]);
-
-onMounted(() => {
-    for (let i = 18; i <= 60; i++) {
-        edades.value.push(i);
-    }
-});
-
-/* 
-Formulario Register - Inputs
-- rol
-- rut
-- nombre
-- apellido
-- edad
-- sexo
-- email
-- contrasena
-- disponibilidad
-*/
-
 const rol = ref("");
 const nombre = ref("");
 const apellido = ref("");
@@ -111,9 +96,59 @@ const rut = ref("");
 const edad = ref("");
 const sexo = ref("");
 const disponibilidad = ref("");
+const institucion = ref("");
+
+const instituciones = ref([]);
+
+const getInstituciones = async () => {
+    try {
+        const response = await axios.get('http://localhost:8090/instituciones/todo');
+        instituciones.value = response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+onMounted(getInstituciones);
+
+const edades = ref([]);
+
+for (let i = 18; i <= 60; i++) {
+    edades.value.push(i);
+}
+
+const registerUser = async () => {
+    if(rol.value === "Voluntario") {
+        const data = {
+            rutVoluntario: rut.value,
+            nombreVoluntario: nombre.value,
+            apellidoVoluntario: apellido.value,
+            edadVoluntario: edad.value, 
+            sexoVoluntario: sexo.value,
+            email: email.value,
+            contrasena: contrasena.value,
+            disponibilidad: disponibilidad.value
+        }
+        console.log("Voluntario")
+        console.log(data);
+        redirectToLogin();
+    } 
+    else if (rol.value === "Coordinador") {
+        const data = {
+            rutCoordinador: rut.value,
+            nombreCoordinador: nombre.value,
+            apellidoCoordinador: apellido.value,
+            email: email.value,
+            contrasena: contrasena.value,
+            idInstitucion: institucion.value
+        }
+        console.log("Coordinador")
+        console.log(data);
+        redirectToLogin();
+    }
+}
 
 const redirectToLogin = () => {
-    // Falta logica
     console.log("Redirigiendo a /");
     router.push('/');
 }
