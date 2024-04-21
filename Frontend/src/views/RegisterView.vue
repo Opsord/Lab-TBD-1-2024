2e59151b1ba7b3b2122311a01ecc5bbb2d1a7142
arenasesteban <esteban.arenas.a@usach.cl>
2024-04-20 23:24:12 -0400

    [Esteban Arenas] Formulario register aplicando grid

--- Frontend/src/views/RegisterView.vue
+++ Frontend/src/views/RegisterView.vue
@@ -5,28 +5,30 @@
         </div>
         <div class="w-full p-8 bg-gray-50 border rounded-xl shadow-md">
             <h2 class="mb-8 text-center">Llena el siguiente formulario</h2>
-            <form class="grid grid-cols-2 space-y-5">
-                <div class="col-span-2 flex flex-col gap-2">
-                    <label for="rut" class="text-sm">RUT</label>
-                    <input type="text" class="px-3 py-2 border border-gray-400 rounded text-sm">
+            <form>
+                <div class="grid gap-5 grid-cols-2">
+                    <div class="flex flex-col gap-2">
+                        <label for="nombre" class="text-sm">Nombre</label>
+                        <input type="text" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="Nombre usuario">
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label for="apellido" class="text-sm">Apellido</label>
+                        <input type="text" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="Apellido usuario">
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label for="correo" class="text-sm">Correo electrónico</label>
+                        <input type="email" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="usuario@email.com">
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label for="contrasena" class="text-sm">Contraseña</label>
+                        <input type="password" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="********">
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <label for="rut" class="text-sm">RUT</label>
+                        <input type="text" class="px-3 py-2 border border-gray-400 rounded text-sm" placeholder="12.345.678-9">
+                    </div>
                 </div>
-                <div class="flex flex-col gap-2">
-                    <label for="nombre" class="text-sm">Nombre</label>
-                    <input type="text" class="px-3 py-2 border border-gray-400 rounded text-sm">
-                </div>
-                <div class="flex flex-col gap-2">
-                    <label for="apellido" class="text-sm">Apellido</label>
-                    <input type="text" class="px-3 py-2 border border-gray-400 rounded text-sm">
-                </div>
-                <div class="flex flex-col gap-2">
-                    <label for="correo" class="text-sm">Correo electrónico</label>
-                    <input type="email" class="px-3 py-2 border border-gray-400 rounded text-sm">
-                </div>
-                <div class="flex flex-col gap-2">
-                    <label for="contrasena" class="text-sm">Contraseña</label>
-                    <input type="password" class="px-3 py-2 border border-gray-400 rounded text-sm">
-                </div>
-                <div class="">
+                <div class="mb-6">
                     <label for="sexo" class="text-sm">Sexo</label>
                     <input type="radio" name="sexo">
                     <label for="masculino" class="text-sm">Masculino</label>
@@ -35,14 +37,14 @@
                     <input type="radio" name="sexo">
                     <label for="no-binario" class="text-sm">No binario</label>
                 </div>
-                <div class="">
+                <div class="mb-6">
                     <label for="edad" class="text-sm">Edad</label>
                     <select>
                         <option value="">Seleccionar</option>
                         <option value=""></option>
                     </select>
                 </div>
-                <div class="">
+                <div class="mb-6">
                     <label for="disponibilidad" class="text-sm">Disponibilidad</label>
                     <input type="radio" name="disponibilidad">
                     <label for="disponibilidad-si" class="text-sm">Sí</label>
@@ -58,6 +60,11 @@
 </template>
 
 <script>
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import axios from 'axios';
+
+const router = useRouter();
 /* 
 Formulario Register - Inputs
 - rut
