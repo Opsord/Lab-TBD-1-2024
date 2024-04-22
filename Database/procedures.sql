32dd8d6abca0eb0a44884e49ad5b06d0e39319ad
Yuyunisse <92490217+Yuyunisse@users.noreply.github.com>
2024-04-22 13:24:22 -0400

    Update procedures.sql

--- Database/procedures.sql
+++ Database/procedures.sql
@@ -8,79 +8,79 @@ BEGIN
     -- Contar operaciones de inserción por voluntarios
     RAISE NOTICE 'Voluntarios con más operaciones de inserción:';
     FOR usuario_voluntario IN
-        SELECT nombre, COUNT(*)
+        SELECT rut, nombre, COUNT(*)
         FROM voluntario_disparador
         WHERE operacion = 'INSERT'
-        GROUP BY nombre
+        GROUP BY rut, nombre
         ORDER BY COUNT(*) DESC
         LIMIT 1
     LOOP
-        RAISE NOTICE 'Usuario: %, Cantidad: %', usuario_voluntario.nombre, usuario_voluntario.count;
+        RAISE NOTICE 'Rut: %, Nombre: %, Cantidad: %', usuario_voluntario.rut, usuario_voluntario.nombre, usuario_voluntario.count;
     END LOOP;
 
     -- Contar operaciones de actualización por voluntarios
     RAISE NOTICE 'Voluntarios con más operaciones de actualización:';
     FOR usuario_voluntario IN
-        SELECT nombre, COUNT(*)
+        SELECT rut, nombre, COUNT(*)
         FROM voluntario_disparador
         WHERE operacion = 'UPDATE'
-        GROUP BY nombre
+        GROUP BY rut, nombre
         ORDER BY COUNT(*) DESC
         LIMIT 1
     LOOP
-        RAISE NOTICE 'Usuario: %, Cantidad: %', usuario_voluntario.nombre, usuario_voluntario.count;
+        RAISE NOTICE 'Rut: %, Nombre: %, Cantidad: %', usuario_voluntario.rut, usuario_voluntario.nombre, usuario_voluntario.count;
     END LOOP;
 
     -- Contar operaciones de eliminación por voluntarios
     RAISE NOTICE 'Voluntarios con más operaciones de eliminación:';
     FOR usuario_voluntario IN
-        SELECT nombre, COUNT(*)
+        SELECT rut, nombre, COUNT(*)
         FROM voluntario_disparador
         WHERE operacion = 'DELETE'
-        GROUP BY nombre
+        GROUP BY rut, nombre
         ORDER BY COUNT(*) DESC
         LIMIT 1
     LOOP
-        RAISE NOTICE 'Usuario: %, Cantidad: %', usuario_voluntario.nombre, usuario_voluntario.count;
+        RAISE NOTICE 'Rut: %, Nombre: %, Cantidad: %', usuario_voluntario.rut, usuario_voluntario.nombre, usuario_voluntario.count;
     END LOOP;
 
     -- Contar operaciones de inserción por coordinadores
     RAISE NOTICE 'Coordinadores con más operaciones de inserción:';
     FOR usuario_coordinador IN
-        SELECT nombre, COUNT(*)
+        SELECT rut, nombre, COUNT(*)
         FROM coordinador_disparador
         WHERE operacion = 'INSERT'
-        GROUP BY nombre
+        GROUP BY rut, nombre
         ORDER BY COUNT(*) DESC
         LIMIT 1
     LOOP
-        RAISE NOTICE 'Usuario: %, Cantidad: %', usuario_coordinador.nombre, usuario_coordinador.count;
+        RAISE NOTICE 'Rut: %, Nombre: %, Cantidad: %', usuario_coordinador.rut, usuario_coordinador.nombre, usuario_coordinador.count;
     END LOOP;
 
     -- Contar operaciones de actualización por coordinadores
     RAISE NOTICE 'Coordinadores con más operaciones de actualización:';
     FOR usuario_coordinador IN
-        SELECT nombre, COUNT(*)
+        SELECT rut, nombre, COUNT(*)
         FROM coordinador_disparador
         WHERE operacion = 'UPDATE'
-        GROUP BY nombre
+        GROUP BY rut, nombre
         ORDER BY COUNT(*) DESC
         LIMIT 1
     LOOP
-        RAISE NOTICE 'Usuario: %, Cantidad: %', usuario_coordinador.nombre, usuario_coordinador.count;
+        RAISE NOTICE 'Rut: %, Nombre: %, Cantidad: %', usuario_coordinador.rut, usuario_coordinador.nombre, usuario_coordinador.count;
     END LOOP;
 
     -- Contar operaciones de eliminación por coordinadores
     RAISE NOTICE 'Coordinadores con más operaciones de eliminación:';
     FOR usuario_coordinador IN
-        SELECT nombre, COUNT(*)
+        SELECT rut, nombre, COUNT(*)
         FROM coordinador_disparador
         WHERE operacion = 'DELETE'
-        GROUP BY nombre
+        GROUP BY rut, nombre
         ORDER BY COUNT(*) DESC
         LIMIT 1
     LOOP
-        RAISE NOTICE 'Usuario: %, Cantidad: %', usuario_coordinador.nombre, usuario_coordinador.count;
+        RAISE NOTICE 'Rut: %, Nombre: %, Cantidad: %', usuario_coordinador.rut, usuario_coordinador.nombre, usuario_coordinador.count;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
